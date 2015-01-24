e5fb8ec9ba0c9ca042b821abba62813518e9f515
MKLau <matthewklau@fas.harvard.edu>
2015-01-24 17:42:21 -0500

    started pulling in station ids

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Pulling weather station data for the Ant Dimensions project.
 
 Main Question: how does chamber temp compare to rural and city temp?
 
+http://blog.revolutionanalytics.com/2014/02/r-and-the-weather.html
+
+Analysis are performed using src/waTemps.R
--- src/waTemp.R
+++ src/waTemp.R
@@ -1,2 +1,19 @@
 ###script for getting weather station data
+library(weatherData)
+rm.space <- function(x,lim=10){
+ out <- x
+ for (i in 1:lim){out <- sub('   ',' ',out)}
+ for (i in 1:lim){out <- sub('  ',' ',out)}
+ return(as.character(out))
+}
 
+###Get station IDs
+ws.ids <- readLines('http://weather.rap.ucar.edu/surface/stations.txt')
+nc.ids <- ws.ids[2161:2253]
+ma.ids <- ws.ids[1609:1651]
+nc.ids <- as.character(sapply(sapply(nc.ids,rm.space),rm.space))
+ma.ids <- as.character(sapply(sapply(ma.ids,rm.space),rm.space))
+nc.ids <- sapply(nc.ids,strsplit,split=' ')
+ma.ids <- sapply(ma.ids,strsplit,split=' ')
+nc.ids <- do.call(rbind,nc.ids)
+ma.ids <- do.call(rbind,ma.ids)
